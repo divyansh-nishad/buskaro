616af7dc1ec9741a75907bcd8ce12a663b676da8
Vedasto <prajesh.eleven118@gmail.com>
2022-11-23 13:15:15 +0530

    - added pages

---
+++ screens/BusListScreen.js
@@ -0,0 +1,123 @@
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { Component, useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const BusListScreen = () => {
+    const nav = useNavigation()
+
+    useLayoutEffect(() => {
+        nav.setOptions({
+            headerShown: false,
+        })
+    }, [nav])
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.navbar}>
+                <TouchableOpacity
+                    style={styles.backBtn}
+                    onPress={() => nav.goBack()}
+                >
+                    <Ionicons name="chevron-back-outline" size={28} color="#2d179b" />
+                </TouchableOpacity>
+            </View>
+            <ScrollView style={styles.busList}>
+                <TouchableOpacity
+                    style={styles.busItem}
+                    onPress={() => nav.navigate('BusBoarding')}
+                >
+                    <View style={styles.busItemLeft}>
+                        <Text style={styles.busName}>Bus 1</Text>
+                        <Text style={styles.busNumber}>UP 8900</Text>
+                        <View style={styles.route}>
+                            <Text style={styles.routeSource}>Kathmandu</Text>
+                            <MaterialIcons name="compare-arrows" size={28} color="#2d179b" />
+                            <Text style={styles.routeDest}>Pokhara</Text>
+                        </View>
+                    </View>
+                    <View style={styles.busItemRight}>
+                        <View style={styles.crowdBox}>
+                            <Text style={styles.crowd}>56%</Text>
+                        </View>
+                        <Text style={styles.price}>Rs. 500</Text>
+                    </View>
+                </TouchableOpacity>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100%',
+        width: '100%',
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+    },
+    navbar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    backBtn: {
+        padding: 12,
+        backgroundColor: '#e1dbff',
+        borderRadius: 10,
+    },
+    busList: {
+        marginVertical: 20,
+    },
+    busItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 20,
+        borderColor: '#dedede',
+        borderWidth: 1,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+    route: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        backgroundColor: '#e1dbff',
+        borderRadius: 50,
+    },
+    busName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    busNumber: {
+        fontSize: 16,
+        marginVertical: 5,
+    },
+    routeSource: {
+        marginRight: 10,
+    },
+    routeDest: {
+        marginLeft: 10,
+    },
+    crowdBox: {
+        backgroundColor: '#e1dbff',
+        borderRadius: 50,
+        padding: 10,
+        alignItems: 'center',
+    },
+    crowd: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#2d179b',
+    },
+    price: {
+        fontSize: 28,
+    },
+})
+
+export default BusListScreen
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,130 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { useLayoutEffect } from 'react'
+import MapView from 'react-native-maps';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
+    const nav = useNavigation()
+
+    useLayoutEffect(() => {
+        nav.setOptions({
+            // headerTitle: 'Home',
+            // headerTitleAlign: 'center',
+            headerShown: false,
+        })
+    }, [nav])
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={styles.container}>
+            <View style={styles.navbar}>
+                <View style={styles.userInfo}>
+                    <Image source={require('../assets/icon.png')}
+                        style={styles.userPhoto} />
+                    <Text style={styles.userName}>@username</Text>
+                </View>
+                <TouchableOpacity style={styles.settingsBtn}>
+                    <AntDesign name="setting" size={28} color="#2d179b" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.mapBox}>
+                <MapView style={styles.map} />
+            </View>
+            <View style={styles.destinationContainer}>
+                <View style={styles.destinationBox}>
+                    <View style={styles.destinationIcon}>
+                        <Ionicons name="ios-location-outline" size={28} color="#2d179b" />
+                    </View>
+                    <View style={styles.inputBox}>
+                        <TextInput style={styles.input} placeholder="Enter your destination" />
+                    </View>
+                </View>
+                <TouchableOpacity
+                    style={styles.searchBtn}
+                    onPress={() => nav.navigate('BusList')}
+                >
+                    <Text style={styles.searchBtnText}>Search</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100%',
+        width: '100%',
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+    },
+    navbar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    userInfo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    userPhoto: {
+        height: 50,
+        width: 50,
+    },
+    userName: {
+        marginLeft: 10,
+        fontSize: 20,
+    },
+    settingsBtn: {
+        padding: 12,
+        backgroundColor: '#e1dbff',
+        borderRadius: 10,
+    },
+    mapBox: {
+        height: '80%',
+        width: '100%',
+        // marginTop: 20,
+        // marginBottom: 20,
+        borderRadius: 10,
+        overflow: 'hidden',
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+    },
+    destinationBox: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: '#dedede',
+        borderWidth: 1,
+        borderRadius: 10,
+    },
+    destinationIcon: {
+        backgroundColor: '#e1dbff',
+        padding: 12,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+    },
+    inputBox: {
+        paddingLeft: 10,
+    },
+    input: {
+        fontSize: 20,
+    },
+    searchBtn: {
+        backgroundColor: '#2d179b',
+        padding: 12,
+        borderRadius: 10,
+        marginTop: 20,
+    },
+    searchBtnText: {
+        color: '#fff',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+})
 
 export default HomeScreen
